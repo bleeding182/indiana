7c65db8390c253ee7ca390d61d0a6bc6d4400409
David Medenjak <david.medenjak@allaboutapps.at>
2020-03-24 17:48:20 +0100

    Bump versions

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -9,7 +9,7 @@ repositories {
 }
 
 dependencies {
-    implementation("com.android.tools.build:gradle:3.5.0")
-    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.3.50")
+    implementation("com.android.tools.build:gradle:3.6.1")
+    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.3.71")
     implementation(gradleApi())
 }
--- buildSrc/src/main/kotlin/at/allaboutapps/build/Dependencies.kt
+++ buildSrc/src/main/kotlin/at/allaboutapps/build/Dependencies.kt
@@ -3,13 +3,13 @@
 // no package so that we don't need an import
 
 object Versions {
-    const val Dagger = "2.25.2"
-    const val Kotlin = "1.3.50"
-    const val Moshi = "1.9.1"
-    const val OkHttp = "3.12.3"
-    const val Retrofit = "2.6.1"
+    const val Dagger = "2.27"
+    const val Kotlin = "1.3.71"
+    const val Moshi = "1.9.2"
+    const val OkHttp = "3.12.10"
+    const val Retrofit = "2.8.0"
     const val Room = "2.1.0"
-    const val RxJava = "2.2.11"
+    const val RxJava = "2.2.19"
     const val LifecycleVersion = "1.1.1"
 }
 

5caa86b40bf678b3fca37f7138aa455cfcb30b5d
David Medenjak <davidmedenjak@gmail.com>
2025-07-03 20:11:00 +0200

    Bump version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,8 +21,8 @@ android {
         applicationId = "com.davidmedenjak.indiana"
         minSdk = 24
         targetSdk = 36
-        versionCode = 14
-        versionName = "1.1.0"
+        versionCode = 15
+        versionName = "1.1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }

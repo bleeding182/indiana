50c8eb4a87ae7cad6524fc4f1aa254332453a61c
David Medenjak <davidmedenjak@gmail.com>
2025-11-09 15:15:09 +0100

    Add preview

--- theme/src/main/java/com/davidmedenjak/indiana/theme/ui/atoms/HorizontalDivider.kt
+++ theme/src/main/java/com/davidmedenjak/indiana/theme/ui/atoms/HorizontalDivider.kt
@@ -1,10 +1,25 @@
 package com.davidmedenjak.indiana.theme.ui.atoms
 
+import androidx.compose.foundation.layout.Column
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.PreviewLightDark
+import com.davidmedenjak.indiana.theme.ui.preview.PreviewSurface
 import androidx.compose.material3.HorizontalDivider as M3HorizontalDivider
 
 @Composable
 fun HorizontalDivider(
-    modifier: Modifier,
+    modifier: Modifier = Modifier,
 ) = M3HorizontalDivider(modifier)
+
+@PreviewLightDark
+@Composable
+private fun Preview() {
+    PreviewSurface {
+        Column {
+            Text("Hello")
+            HorizontalDivider()
+            Text("World")
+        }
+    }
+}
